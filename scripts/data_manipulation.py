38a2ba4823dc7f3a26c668c1ce62419f86e7f095
robert-f-donohue <robert.donohue@envien-studio.com>
2024-05-23 15:21:42 -0400

    Created directories and starter files

---
+++ scripts/data_manipulation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Conversions to mmBtu for Different Fuel Types
+kWh_to_MMBTU = 0.003412
+THERM_to_MMBTU = 0.1
+FO1_Gal_to_MMBTU = 0.135
+FO2_Gal_to_MMBTU = 0.14
+FO4_Gal_to_MMBTU = 0.146
+DIESEL_Gal_to_MMBTU = 0.1387
+
+
