d9d95efc30c9d7f88ed231a733caa89ae4193be5
robert-f-donohue <robert.donohue@envien-studio.com>
2024-06-10 16:17:51 -0400

    Data has been split into reported 2022 and 2023 data as well as never reported data

--- scripts/data_manipulation.py
+++ scripts/data_manipulation.py
@@ -53,6 +53,26 @@ def filter_empty_datasets(group):
         return group
 
 
+def check_for_duplicates(df, column_name):
+    """
+    Checks for duplicate values in the specified column of the DataFrame
+    and prints the unique duplicated values.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame to check for duplicates.
+    column_name (str): The name of the column to check for duplicates.
+
+    Returns:
+    list: A list of unique duplicated values.
+    """
+    duplicate_mask = df[column_name].duplicated(keep=False)
+    duplicate_values = df[column_name][duplicate_mask].unique()
+    print(f"\nDuplicated {column_name} values:")
+    for value in duplicate_values:
+        print(value)
+    return list(duplicate_values)
+
+
 # File paths to BERDO Data 2021-2023
 file_path_2022 = '../data-files/0-berdo-raw-data-2022.csv'
 file_path_2023 = '../data-files/0-berdo-raw-data-2023.csv'
@@ -61,13 +81,9 @@ file_path_2023 = '../data-files/0-berdo-raw-data-2023.csv'
 df_2022 = pd.read_csv(file_path_2022, encoding='ISO-8859-1', skipinitialspace=True)
 df_2023 = pd.read_csv(file_path_2023, encoding='ISO-8859-1', skipinitialspace=True)
 
-
-
 # Clean columns of leading characters
 df_2022.columns = df_2022.columns.str.strip().str.replace('\n', '').str.replace('\r', '')
 df_2023.columns = df_2023.columns.str.strip().str.replace('\n', '').str.replace('\r', '')
-
-# df_2023.to_csv('../data-files/test.csv', index=False)
 
 # Columns to remove from data
 columns_to_drop_2023 = ['Building Address City', 'Parcel Address City', 'Reported Enclosed Parking Area (Sq Ft)',
@@ -147,10 +163,14 @@ df_berdo_reported_2023 = pd.concat(berdo_reported_list_2023)
 df_berdo_not_reported_2023 = pd.concat(berdo_not_reported_list_2023)
 
 # Add a Data Year column to df_berdo_reported and make it 2023
-df_berdo_reported_2023['Data Year'] = 2023
+df_berdo_reported_2023['Data Year'] = 2022
 
 # Take not reported data from 2023 and join those BERDO IDs on the 2022 data
 df_berdo_2022 = df_berdo_not_reported_2023[['BERDO ID']].merge(df_clean_2022, how='inner')
+
+# Find the BERDO IDs that were never reported in either dataset
+merged_ids = df_berdo_2022['BERDO ID'].unique()
+df_berdo_never_reported_1 = df_berdo_not_reported_2023[~df_berdo_not_reported_2023['BERDO ID'].isin(merged_ids)]
 
 # Group 2022 data so that it can be iterated over for separation
 df_grouped_2022 = df_berdo_2022.sort_values(by='BERDO ID', ascending=True).groupby('BERDO ID')
@@ -171,39 +191,77 @@ for name, group in df_grouped_2022:
 df_berdo_reported_2022 = pd.concat(berdo_reported_list_2022)
 df_berdo_not_reported_2022 = pd.concat(berdo_not_reported_list_2022)
 
-# Add a Data Year column to df_berdo_reported and make it 2023
-df_berdo_reported_2022['Data Year'] = 2022
+# Add a Data Year column to df_berdo_reported and make it 2021
+df_berdo_reported_2022['Data Year'] = 2021
+
+# Concatenate the data that was reported in neither 2022 or 2023
+df_berdo_never_reported = pd.concat([df_berdo_never_reported_1, df_berdo_not_reported_2022], axis=0)
+df_berdo_never_reported = df_berdo_never_reported.sort_values(by='BERDO ID', ascending=True)
+
+# Add indexes to the reported BERDO data so that they can be sorted to remove duplicate values
+df_berdo_reported_2022['_id'] = df_berdo_reported_2022.index
+df_berdo_reported_2023['_id'] = df_berdo_reported_2023.index
+
+# # Get rid of all duplicate BERDO IDs in the 2023 datasets
+# Gets rid of duplicate condition 500 Walk Hill St
+df_berdo_reported_2023 = df_berdo_reported_2023[df_berdo_reported_2023['_id'] != 2506]
+
+# Corrects the BERDO ID for Tudor Place & adds necessary data
+df_berdo_reported_2023.loc[df_berdo_reported_2023['_id'] == 5958, 'BERDO ID'] = 106338
+df_berdo_reported_2023.loc[df_berdo_reported_2023['_id'] == 5958, 'Property Owner Name'] = 'TUDOR PLACE CONDO TRUST'
+
+# Add property owner name to BERDO ID 105780 and delete the unneeded row
+df_berdo_reported_2023.loc[df_berdo_reported_2023['_id'] == 5948, 'Property Owner Name'] = 'SIMMONS UNIVERSITY'
+df_berdo_reported_2023 = df_berdo_reported_2023[df_berdo_reported_2023['_id'] != 2733]
+
+# Add blank columns to 154 Ruskindale Rd and delete incomplete column
+ruskindale_new_values = {
+    'Property Owner Name': 'CITY OF BOSTON',
+    'Tax Parcel ID': 1803703000,
+    'Building Address': '154-164 Ruskindale Rd',
+    'Parcel Address': '154 156 ruskindale rd',
+    'Parcel Address Zip Code': 2136
+}
+
+df_berdo_reported_2023.loc[df_berdo_reported_2023['_id'] == 5948, ['Property Owner Name', 'Tax Parcel ID',
+                                                                   'Building Address', 'Parcel Address',
+                                                                   'Parcel Address Zip Code']]\
+                                                                    = ruskindale_new_values.values()
+
+df_berdo_reported_2023 = df_berdo_reported_2023[df_berdo_reported_2023['_id'] != 638]
+
+# Drop column for 150 Mass Ave that appears to be a single building in a campus
+df_berdo_reported_2023 = df_berdo_reported_2023[df_berdo_reported_2023['_id'] != 5917]
+
+# # Get rid of all duplicate BERDO IDs in the 2022 datasets
+# Get rid of duplicates for 1910-1920 Centre St
+df_berdo_reported_2022 = df_berdo_reported_2022[df_berdo_reported_2022['_id'] != 1901]
+
+# Get rid of duplicates for 52-68 Highland Park
+df_berdo_reported_2022 = df_berdo_reported_2022[df_berdo_reported_2022['_id'] != 2312]
 
 # Check number of reported and non-reported data so that the total matches the total number of properties
 print(df_clean_2023['BERDO ID'].nunique())
 print(df_clean_2023.shape)
 print(df_berdo_reported_2023['BERDO ID'].nunique())
 print(df_berdo_reported_2023.shape)
-print(df_berdo_not_reported_2023['BERDO ID'].nunique())
-print(df_berdo_not_reported_2023.shape)
 print(df_berdo_reported_2022['BERDO ID'].nunique())
 print(df_berdo_reported_2022.shape)
-print(df_berdo_not_reported_2022['BERDO ID'].nunique())
-print(df_berdo_not_reported_2022.shape)
+print(df_berdo_never_reported['BERDO ID'].nunique())
+print(df_berdo_never_reported.shape)
 
-# # Download data into separate .csv files
-# df_berdo_reported_2023.to_csv('../data-files/2-berdo_reported_2023.csv', index=False)
-# df_berdo_not_reported_2023.to_csv('../data-files/3-berdo_not_reported_2023.csv', index=False)
+# Remove unique identifiers used for filtering duplicates
+df_berdo_reported_2022.drop('_id', axis=1, inplace=True)
+df_berdo_reported_2023.drop('_id', axis=1, inplace=True)
 
-duplicate_mask_2023 = df_clean_2023['BERDO ID'].duplicated(keep=False)
-
-duplicate_values = df_clean_2023['BERDO ID'][duplicate_mask_2023].unique()
-print("\nDuplicated BERDO IDs column 2023:")
-for value in duplicate_values:
-    print(value)
-
+# Download data into separate .csv files
+df_berdo_reported_2022.to_csv('../data-files/2-berdo_reported_2022.csv', index=False)
+df_berdo_reported_2023.to_csv('../data-files/2-berdo_reported_2023.csv', index=False)
+df_berdo_never_reported.to_csv('../data-files/2-berdo_never_reported.csv', index=False)
 
 # # Checking for duplicate values in dataset
-duplicate_mask = df_berdo_2022['BERDO ID'].duplicated(keep=False)
+duplicate_ids_2022 = check_for_duplicates(df_berdo_reported_2022, 'BERDO ID')
+duplicate_ids_2023 = check_for_duplicates(df_berdo_reported_2023, 'BERDO ID')
 
-duplicate_values = df_berdo_2022['BERDO ID'][duplicate_mask].unique()
-print("\nDuplicated BERDO IDs column 2022:")
-for value in duplicate_values:
-    print(value)
 
 
