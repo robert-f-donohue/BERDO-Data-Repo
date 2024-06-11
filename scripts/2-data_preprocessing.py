Fixed bug in emissions calculation and updated emissions factors to ISO NE for 2022 and 2023

scripts/2-data_preprocessing.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+
+def clean_cell(cell):
+    # Strip white space
+    cell = cell.strip()
+    # Replace cell with 0 if it is just a comma
+    if cell == ',':
+        return 0
+    # Convert to integer if possible
+    try:
+        return int(cell)
+    except ValueError:
+        return cell
+
 # File paths to separated BERDO data and emissions factors for all fuel types each year through 2050
 file_path_2022 = '../data-files/2-berdo_reported_2022.csv'
 file_path_2023 = '../data-files/2-berdo_reported_2023.csv'
@@ -8,24 +21,24 @@ file_path_emissions = '../data-files/1-emissions-factors.csv'
 # DataFrames for BERDO data and emissions factors
 df_berdo_reported_2022 = pd.read_csv(file_path_2022)
 df_berdo_reported_2023 = pd.read_csv(file_path_2023)
-berdo_emissions_factors = pd.read_csv(file_path_emissions)
+df_berdo_emissions_factors = pd.read_csv(file_path_emissions)
 
 # Concatenated DataFrame for all BERDO Data
 df_berdo_data = pd.concat([df_berdo_reported_2022, df_berdo_reported_2023], axis=0)
 
 # Merged DataFrame including emissions factors data
-df_berdo = pd.merge(df_berdo_data, berdo_emissions_factors, on=['Data Year'], how='left')
-
-print(df_berdo.head())
-print(df_berdo.shape)
+df_berdo = pd.merge(df_berdo_data, df_berdo_emissions_factors, on=['Data Year'], how='left')
 
 # Remove unneeded columns
 df_berdo = df_berdo.drop(['Estimated Total GHG Emissions (kgCO2e)', 'Cooresponding Campus ID'], axis=1)
 
+# Fill in all NaN's with 0's
+df_berdo.fillna(0, inplace=True)
+
 # Add columns for emissions by fuel source for each Data Year/BERDO ID
-df_berdo['Electricity Emissions (MT CO2e)'] = (((df_berdo['Electricity Usage (kBtu)']
-                                                 - df_berdo['Renewable System Electricity Usage Onsite (kBtu)']) / 1000)
-                                               * df_berdo['Electricity Emissions']) / 1000
+df_berdo['Electricity Emissions (MT CO2e)'] = (
+    (df_berdo['Electricity Usage (kBtu)'] - df_berdo['Renewable System Electricity Usage Onsite (kBtu)']) / 1000
+) * df_berdo['Electricity Emissions'] / 1000
 
 df_berdo['Natural Gas Emissions (MT CO2e)'] = ((df_berdo['Natural Gas Usage (kBtu)'] / 1000)
                                                * df_berdo['Natural Gas Emissions']) / 1000
@@ -72,5 +85,21 @@ GHG_emissions_columns = ['Electricity Emissions (MT CO2e)', 'Natural Gas Emissio
 df_berdo['Total GHG Emissions (MT CO2e)'] = df_berdo[GHG_emissions_columns].sum(axis=1)
 df_berdo['Total GHG Emissions (MT CO2e)'] = df_berdo['Total GHG Emissions (MT CO2e)'].round().astype(int)
 
-df_berdo.to_csv('../data-files/preprocessed-data/berdo-emissions-data.csv')
+# Clean DataFrame of all whitespace and replace columns with just a comma with a zero
+df_berdo = df_berdo.applymap(clean_cell)
+
+# Remove rows with no value for Largest Property Type and replace it with All Property Types column
+df_berdo['Largest Property Type'] = df_berdo.apply(
+    lambda row: row['All Property Types'] if row['Largest Property Type'] == 0 else row['Largest Property Type'],
+    axis=1)
+
+# If All Property Types is now the same as Largest Property Type, then zero out the All Property Types
+df_berdo['All Property Types'] = df_berdo.apply(
+    lambda row: 0 if row['Largest Property Type'] == row['All Property Types'] else row['All Property Types'],
+    axis=1)
+
+
+df_berdo = df_berdo.sort_values(by=['Largest Property Type', 'All Property Types'], ascending=True)
+
+df_berdo.to_csv('../data-files/preprocessed-data/berdo-emissions-data.csv', index=False)
 